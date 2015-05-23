c0b344e904f64cfe21a022e0c86927e0956f712f
hanserikbratt <hebratt@gmail.com>
2015-05-23 14:58:32 +0200

    inga ändringar alls nästan i rest exemplet

--- projekt/restCall.py
+++ projekt/restCall.py
@@ -1,19 +1,16 @@
 import requests
 import json
-'''
-url = "http://localhost:8080/projekt/webresources/restservices.products"
-headers = {'accept' : 'application/json'}
-r = requests.get(url,headers=headers)
-print(r.json())
-#print(r.status_code)
-#print(r.text)
-print(r.headers)
-'''
-url2 = "http://localhost:8080/projekt/webresources/restservices.products/1"
-dr = requests.delete(url2)
-print(dr.headers)
 
 url = "http://localhost:8080/projekt/webresources/restservices.products"
 headers = {'accept' : 'application/json'}
 r = requests.get(url,headers=headers)
-print(r.json())
+print(r.text)
+'''
+#print(r.status_code)
+#print(r.text)
+print(r.headers)
+
+url2 = "http://localhost:8080/projekt/webresources/restservices.products/1"
+dr = requests.delete(url2)
+print(dr.headers)
+'''
